2bd6a565530abcaf536398f9fa886870f34ddb95
Oleg Rashinskiy <oleg.rashinskiy@gmail.com>
2020-03-16 19:17:02 +0300

    init: finish morse-decoder

--- src/index.js
+++ src/index.js
@@ -35,10 +35,36 @@ const MORSE_TABLE = {
     '---..':  '8',
     '----.':  '9',
     '-----':  '0',
+    ' ': ' ',
 };
 
 function decode(expr) {
-    // write your solution here
+    let startPoint = 0;
+    let decodeWord = '';
+    let codeSymbols = [];
+    for(let i = 0; i < expr.length/10; i++){
+        codeSymbols.push(expr.slice(startPoint, startPoint+10));
+        startPoint += 10;
+    }
+
+    for(let i = 0; i < codeSymbols.length ; i++){
+        let codeWord = [];
+        if (codeSymbols[i] == '**********') {
+            codeSymbols[i] = ' ';
+        }else{
+        for(let j = codeSymbols[i].length; j > 0; j -= 2){
+            switch(codeSymbols[i].slice(j-2, j)){
+                case '10' : codeWord.push('.'); break;
+                case '11' : codeWord.push('-'); break;
+                case '00' : codeWord.push(''); break;
+                default: break;
+            }
+        }
+        codeSymbols[i] = codeWord.reverse().join('');
+    }
+    decodeWord += MORSE_TABLE[codeSymbols[i]];
+    }
+    return decodeWord;
 }
 
 module.exports = {
